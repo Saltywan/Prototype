acaf136297a79cf70c96b3e90d40f1267c862ac7
Saltywan <thanapolwan79@gmail.com>
2022-06-25 23:16:52 +0700

    Responsive?

--- results.js
+++ results.js
@@ -2,6 +2,7 @@ function openform(){
     document.getElementById("container-1").style.display="none";
     document.getElementById("pop").style.display="block";
     document.getElementById("pop").style.opacity=1;
+    //document.getElementById("body1").style.filter="blur(10px)";
 }
 function closeform(){
     document.getElementById("pop").style.display="none";
